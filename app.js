5572551ed17d8fe3a83288df30b26ca4a5458c53
Zhenya Pastukhov <amamahahatala@hotmail.com>
2022-12-21 20:51:16 +0200

    done

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
+// const express = require('express')
+// const logger = require('morgan')
+// const cors = require('cors')
+// require('dotenv').config()
+
+// const app = express
+
+// const { connectMongo } = require('./db/connections')
+
+// const contactsRouter = require('./routes/api/contacts')
+
+// const PORT = process.env.PORT || 3000
+
+// const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
+
+// app.use(logger(formatsLogger))
+// app.use(cors())
+// app.use(express.json())
+
+// app.use('/api/contacts', contactsRouter)
+// app.use((error, req, res, next) => {
+//   res.status(500).json({ message: error.message })
+// })
+
+// const start = async () => {
+//   try {
+//     await connectMongo();
+//     app.listen(PORT, (err) => {
+//       if (err) console.error(`Error at aserver launch: ${err}`)
+//       console.log(`Server running. Use our API on port: ${PORT}`)
+//     })
+//   } catch (err) {
+//     console.error(`Failed to connect`)
+//   }
+// }
+
+// start()
+
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+require("dotenv").config();
 
 const contactsRouter = require('./routes/api/contacts')
+
 
 const app = express()
 
@@ -19,7 +59,9 @@ app.use((req, res) => {
 })
 
 app.use((err, req, res, next) => {
-  res.status(500).json({ message: err.message })
+  // eslint-disable-next-line no-unused-vars
+  const {status = 500, message = "Server error"} = err;
+  res.status(status).json({ message: err.message })
 })
 
 module.exports = app
--- models/contacts.js
+++ models/contacts.js
@@ -1,19 +1,89 @@
-// const fs = require('fs/promises')
+// // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+// const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+// const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+// const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+// const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+// const updateContact = async (contactId, body) => {}
+
+// module.exports = {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// }
+const {Schema, model} = require("mongoose");
+const Joi = require("joi");
+
+
+const userSchema = Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 2,
+        maxlength: 10
+    },
+    email: {
+      type: String,
+      // required: [true, "price must be exist"],
+        required: true,
+        // min: 0.01
+    },
+    phone: {
+        type: Number,
+        required: true,
+        minlength: 2,
+        maxlength: 10
+  },
+    favorite: {
+        type: Boolean,
+        default: true
+    },
+
+}, {versionKey: false, timestamps: true});
+
+const joiSchema = Joi.object({
+    name: Joi.string().alphanum().min(2).max(10).required(),
+    email: Joi.string().email({minDomainSegments: 2,tlds: { allow: ['com', 'net'] }
+      }).required(),
+  phone: Joi.number().min(7).required(),
+    favorite: Joi.bool(),
+    // status: Joi.string().valid("basic", "sale", "stock"),
+    // code: Joi.string().pattern(codeRegexp)
+});
+
+// const statusJoiSchema = Joi.object({
+//     status: Joi.string().valid("basic", "sale", "stock").required()
+// })
+
+const Contact = model("contact", userSchema);
 
 module.exports = {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+    Contact,
+    joiSchema,
+    // statusJoiSchema
 }
+
+//   const result = schema.validate(req.body)
+//   if (result.error) {
+//   return res.status(400).json({ message: result.error.details})
+//   }
+//       const {
+//     name,
+//     email,
+//     phone,
+//   } = req.body
+  
+//   contacts.push({
+//     id:Math.random().toFixed(2),
+//     name,
+//     email,
+//     phone,
+//   })
+//   res.status(201).json({ contacts, message: 'New contact was created' })
+// })
