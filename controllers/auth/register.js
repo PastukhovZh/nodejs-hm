7ff036e3892b7b4c0b789165a8382adec9712a75
Zhenya Pastukhov <123202378+PastukhovZ@users.noreply.github.com>
2023-01-24 10:26:32 +0200

    hw-05

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,27 +1,30 @@
-const {Conflict} = require("http-errors");
+const { Conflict } = require("http-errors");
+const gravatar = require("gravatar");
 
-const {User} = require("../../models");
+const { User } = require("../../models");
 
-const register = async(req, res)=> {
-    const {name, email, password} = req.body;
-    const user = await User.findOne({email});
-    if(user){
-        throw new Conflict(`User with ${email} already exist`)
-    }
-    const newUser = new User({name, email});
-    newUser.setPassword(password);
-    newUser.save();
+const register = async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new Conflict(`User with ${email} already exist`);
+  }
+  const avatarURL = gravatar.url(email);
+  const newUser = new User({ name, email });
+  newUser.setPassword(password);
+  newUser.save();
 
-    res.status(201).json({
-        status: "success",
-        code: 201,
-        data: {
-            user: {
-                email,
-                password
-            }
-        }
-    });
-}
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: {
+      user: {
+        email,
+        password,
+        avatarURL,
+      },
+    },
+  });
+};
 
 module.exports = register;
